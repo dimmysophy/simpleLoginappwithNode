48909a0bdc5fd1f73037239fb804e530325d038c
dimmysophy <agubatachidimma@gmail.com>
2022-11-13 19:13:57 +0000

    added config file

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-const mysql = require('mysql')
 const express = require('express')
 const session = require('express-session')
 const path = require('path')
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'nodelogin',
-})
+const connection = require('./config')
 
 const app = express()
 app.use(
